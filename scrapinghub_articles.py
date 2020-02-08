07ad56a17777649b1eb380050560b33e715b98b8
Brian Cohan <briancohan@gmail.com>
2020-02-07 22:31:37 -0500

    converted tabs to spaces

--- scrapinghub_articles.py
+++ scrapinghub_articles.py
@@ -13,28 +13,28 @@ articles = []
 page = 0
 
 while True:
-	page += 1
-	url = f"https://blog.scrapinghub.com/page/{page}"
-	print(url)
-	response = requests.get(url)
-	soup = BeautifulSoup(response.content, 'html.parser')
+    page += 1
+    url = f"https://blog.scrapinghub.com/page/{page}"
+    print(url)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-	for post in soup.find_all('div', class_='post-item'):
-		title = post.find('h2')
-		link = title.find('a')
-		date = post.find('span', class_='date').text.strip()
+    for post in soup.find_all('div', class_='post-item'):
+        title = post.find('h2')
+        link = title.find('a')
+        date = post.find('span', class_='date').text.strip()
 
-		articles.append({
-			'title': link.text, 
-			'url': link['href'], 
-			'date': datetime.strptime(date, '%B %d, %Y'),
-			})
+        articles.append({
+            'title': link.text, 
+            'url': link['href'], 
+            'date': datetime.strptime(date, '%B %d, %Y'),
+        })
 
-	if soup.find('a', class_='next-posts-link') is None:
-		break
+    if soup.find('a', class_='next-posts-link') is None:
+        break
 
 with open('scrapinghub_articles.md', 'w') as f:
-	f.write("# Scraping Hub Articles\n\n")
-	for article in articles:
-		if article['title'] and article['url']:
-			f.write(f"* {article['date'].strftime('%Y-%m-%d')} [{article['title']}]({article['url']})\n")
+    f.write("# Scraping Hub Articles\n\n")
+    for article in articles:
+        if article['title'] and article['url']:
+            f.write(f"* {article['date'].strftime('%Y-%m-%d')} [{article['title']}]({article['url']})\n")
